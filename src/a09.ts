672c2178f742852b942fa3f94a406b7ee4de82be
Dominik Bathon <dominik.bathon@gmail.com>
2023-12-09 10:56:50 +0100

    Solve day 9

--- src/a09.ts
+++ src/a09.ts
@@ -1,5 +1,18 @@
-import { p, readLines } from "./util/util";
+import { p, parseInts, readLines, sum } from "./util/util";
 
 const lines = readLines("input/a09.txt");
 
-p(lines);
+function predict(numbers: number[]): number {
+  if (numbers.every((n) => n === 0)) {
+    return 0;
+  }
+  const diff: number[] = [];
+  for (let i = 1; i < numbers.length; i++) {
+    diff.push(numbers[i] - numbers[i - 1]);
+  }
+  return numbers.at(-1)! + predict(diff);
+}
+
+p(sum(lines.map((line) => predict(parseInts(line)))));
+
+p(sum(lines.map((line) => predict(parseInts(line).reverse()))));
