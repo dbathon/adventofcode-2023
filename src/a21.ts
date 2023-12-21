067682b83a2c6f4ed5bfe918e88e346f4af752fc
Dominik Bathon <dominik.bathon@gmail.com>
2023-12-21 22:44:43 +0100

    Solve day 21 part 1

--- src/a21.ts
+++ src/a21.ts
@@ -1,5 +1,26 @@
+import { Map2D, Map2DNode } from "./util/map2D";
 import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a21.txt");
 
-p(lines);
+const map = Map2D.fromLines(lines, (c) => c);
+
+const starts = map.getNodes().filter((node) => node.value === "S");
+
+function simulateWalk(starts: Map2DNode<string>[], steps: number): Map2DNode<string>[] {
+  let currentNodes = starts;
+  for (let i = 0; i < steps; i++) {
+    const reachedNodes = new Map<string, Map2DNode<string>>();
+
+    currentNodes.forEach((node) => {
+      node.fourNeighbors
+        .filter((node) => (node.value ?? "#") !== "#")
+        .forEach((node) => reachedNodes.set(node.nodeKey, node));
+    });
+
+    currentNodes = [...reachedNodes.values()];
+  }
+  return currentNodes;
+}
+
+p(simulateWalk(starts, 64).length);
