d1428a71b55ce7cf8621f3495c322c3da9cfbd64
Dominik Bathon <dominik.bathon@gmail.com>
2023-12-22 00:00:27 +0100

    Refactor day 21 for part 2

--- src/a21.ts
+++ src/a21.ts
@@ -7,20 +7,48 @@ const map = Map2D.fromLines(lines, (c) => c);
 
 const starts = map.getNodes().filter((node) => node.value === "S");
 
-function simulateWalk(starts: Map2DNode<string>[], steps: number): Map2DNode<string>[] {
-  let currentNodes = starts;
-  for (let i = 0; i < steps; i++) {
-    const reachedNodes = new Map<string, Map2DNode<string>>();
-
-    currentNodes.forEach((node) => {
-      node.fourNeighbors
-        .filter((node) => (node.value ?? "#") !== "#")
-        .forEach((node) => reachedNodes.set(node.nodeKey, node));
-    });
-
-    currentNodes = [...reachedNodes.values()];
-  }
-  return currentNodes;
+interface Position {
+  readonly x: number;
+  readonly y: number;
 }
 
-p(simulateWalk(starts, 64).length);
+function positionKey(pos: Position): string {
+  return pos.x + ";" + pos.y;
+}
+
+function fourNeighbors({ x, y }: Position): Position[] {
+  return [
+    { x: x + 1, y },
+    { x: x - 1, y },
+    { x, y: y + 1 },
+    { x, y: y - 1 },
+  ];
+}
+
+function simulateWalk(starts: Map2DNode<string>[], steps: number): number[] {
+  let currentPositions: Position[] = starts.map((node) => ({ x: node.x, y: node.y }));
+  const w = map.width;
+  const h = map.height;
+  const counts: number[] = [];
+  for (let i = 0; i < steps; i++) {
+    const reachedPositions = new Map<string, Position>();
+
+    currentPositions.forEach((position) => {
+      fourNeighbors(position)
+        .filter(({ x, y }) => (map.get(((x % w) + w) % w, ((y % h) + h) % h) ?? "#") !== "#")
+        .forEach((position) => reachedPositions.set(positionKey(position), position));
+    });
+
+    currentPositions = [...reachedPositions.values()];
+
+    counts.push(currentPositions.length);
+  }
+  return counts;
+}
+
+const counts = simulateWalk(starts, 64);
+p(counts[63]);
+
+// for (let i = 1; i < counts.length; i++) {
+//   p([counts[i] - counts[i - 1], i, counts[i]]);
+// }
