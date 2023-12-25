d22ce025a0c4dd38c2929ad918ae577557a28847
Dominik Bathon <dominik.bathon@gmail.com>
2023-12-25 12:19:26 +0100

    Solve day 25

--- src/a25.ts
+++ src/a25.ts
@@ -1,5 +1,78 @@
-import { p, readLines } from "./util/util";
+import { Node, dijkstraSearch } from "./util/graphUtil";
+import { findMax, p, readLines } from "./util/util";
 
 const lines = readLines("input/a25.txt");
 
-p(lines);
+const nodeToEdges = new Map<string, Map<string, string>>();
+
+function getEdges(from: string): Map<string, string> {
+  const edges = nodeToEdges.get(from) ?? new Map();
+  nodeToEdges.set(from, edges);
+  return edges;
+}
+
+lines.forEach((line) => {
+  const [from, ...tos] = line.split(/:? /);
+  const edges = getEdges(from);
+  for (const to of tos) {
+    const edgeName = from + "-" + to;
+    edges.set(edgeName, to);
+    getEdges(to).set(edgeName, from);
+  }
+});
+
+const nodes = [...nodeToEdges.keys()];
+
+interface EdgeWithPrev extends Node {
+  nodeKey: string;
+  edge?: string;
+  prev?: EdgeWithPrev;
+}
+
+function findMostUsedEdge(blacklist: Set<string>): { reachCounts: number[]; mostUsed: string | undefined } {
+  const useCount = new Map<string, number>();
+  let reachCounts = new Set<number>();
+
+  for (const startNode of nodes) {
+    const reached: EdgeWithPrev[] = [];
+
+    dijkstraSearch(
+      (node: EdgeWithPrev, _, produceNode) => {
+        reached.push(node);
+        for (const [edge, to] of nodeToEdges.get(node.nodeKey)?.entries() ?? []) {
+          if (!blacklist.has(edge)) {
+            produceNode({ nodeKey: to, edge, prev: node }, 1);
+          }
+        }
+      },
+      { nodeKey: startNode }
+    );
+
+    reachCounts.add(reached.length);
+
+    for (const node of reached) {
+      let current: EdgeWithPrev | undefined = node;
+      while (current && current.edge) {
+        useCount.set(current.edge, (useCount.get(current.edge) ?? 0) + 1);
+        current = current.prev;
+      }
+    }
+  }
+
+  return {
+    reachCounts: [...reachCounts],
+    mostUsed: findMax([...useCount.entries()], (e) => e[1]).maxElement?.[0],
+  };
+}
+
+const blacklist = new Set<string>();
+
+while (true) {
+  const { reachCounts, mostUsed } = findMostUsedEdge(blacklist);
+  if (reachCounts.length === 1) {
+    blacklist.add(mostUsed!);
+  } else {
+    p(reachCounts.reduce((a, b) => a * b, 1));
+    break;
+  }
+}
